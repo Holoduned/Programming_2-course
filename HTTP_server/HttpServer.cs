3b6c26d31bbbff5f4ed6036bb792e2adfed1df86
Holoduned <94117859+Holoduned@users.noreply.github.com>
2022-10-12 09:09:31 +0300

    08_10_2022 (need to fix file)

--- HTTP_server/HttpServer.cs
+++ HTTP_server/HttpServer.cs
@@ -1,91 +1,111 @@
 ﻿using System;
-using System.Net;
+using System.Collections.Generic;
 using System.IO;
-using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
-namespace NetConsoleApp
+namespace Http_Server
 {
-    class HttpServer
+    public enum ServerStatus{Start,Stop};
+    public class ServerSetting
     {
-        private HttpListener listener;
-        public void Listen()
+        public int Port { get; set; } = 7700;
+        //public string Path { get; set; } = @"./site";
+        public string Path { get; set; } = Directory.GetCurrentDirectory();
+    }
+    public class HttpServer:IDisposable
+    {
+        public ServerStatus Status = ServerStatus.Stop;
+        private ServerSetting _serverSetting=new ServerSetting();
+        private readonly HttpListener _httpListener;
+        public HttpServer() 
         {
-            string path = Directory.GetCurrentDirectory();
-            if (!File.Exists(path + "\\google\\index.html"))
-            {
-                Console.WriteLine("Обнаружена ошибка");
-                Stop();
-                Console.WriteLine("Файл html не найден, пожалуйста вложите файл в \\google\\ директорию");
-
-                while (true)
-                {
-                    Console.WriteLine($"Введите {"Start"} для запуска сервера");
-                    var input = Console.ReadLine();
-                    if (input == "Start")
-                    {
-                        if (File.Exists(path + "\\google\\index.html"))
-                        { Console.Clear(); Start(); break; }
-                        Console.WriteLine("Файл html не найден, пожалуйста вложите файл в \\google\\ директорию");
-                    };
-                }
-                
-            }
-            while (true)
-            {
-                HttpListenerContext context = listener.GetContext();
-
-                HttpListenerRequest request = context.Request;
-                HttpListenerResponse response = context.Response;
-
-                response.Headers.Set("Content-Type", "text/html");
-                byte[] buffer = getFile(context.Request.RawUrl.Replace("%20", " "));
-                //response.ContentLength64 = buffer.Length;
-
-                Stream output = response.OutputStream;
-                output.Write(buffer, 0, buffer.Length);
-                output.Close();
-
-                Console.WriteLine("Обработка подключений завершена");
-            }
+            _httpListener = new HttpListener();
+            _httpListener.Prefixes.Add($"http://localhost:"+ _serverSetting.Port + "/");
         }
-
-        public void Start()
-        {
-            Console.WriteLine("Запуск сервера..");
-
-            listener = new HttpListener();
-            listener.Prefixes.Add("http://localhost:8888/google/");
-            listener.Start();
-
+        public void Start() 
+        { 
+             if(Status==ServerStatus.Start)
+            {
+                Console.WriteLine("Сервер уже запущен");
+                return;
+            }
+            Console.WriteLine("Запуск сервера...");
+            _httpListener.Start();
             Console.WriteLine("Сервер запущен");
-            Listen();
+            Status = ServerStatus.Start;
+            Listening();
         }
-
-        public void Stop()
+        public void Stop() 
         {
-            listener.Stop();
+            if (Status == ServerStatus.Stop) return;
+            Console.WriteLine("Остановка сервера...");
+            _httpListener.Stop();
+            Status = ServerStatus.Stop;
             Console.WriteLine("Сервер остановлен");
         }
+        private void Listening()
+        {
+            _httpListener.BeginGetContext(new AsyncCallback(ListenerCallBack), _httpListener);
+        }
+        private void ListenerCallBack(IAsyncResult result) 
+        {
+            if(_httpListener.IsListening)
+            {
+                var _httpContext = _httpListener.EndGetContext(result);
+                HttpListenerRequest request = _httpContext.Request;
+                HttpListenerResponse response = _httpContext.Response;
 
-        private byte[] getFile(string rawUrl)
+                byte[] buffer;
+                if(File.Exists(_serverSetting.Path + "\\google\\index.html"))
+                {
+                    buffer = getFile(_httpContext.Request.RawUrl.Replace("%20", " "));
+                    
+                    if (buffer==null)
+                    {
+                        response.Headers.Set("Content-Type", "text/plain");
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        string err = "404 - not found";
+                        buffer = Encoding.UTF8.GetBytes(err);
+                    }
+                }
+                else
+                {
+                    string err = $" File not found";
+                    buffer = Encoding.UTF8.GetBytes(err);
+                }
+                Stream output = response.OutputStream;
+                output.Write(buffer,0,buffer.Length);
+                output.Close();
+                Listening();
+            }
+        }
+        private byte[] getFile(string rawUrl) 
         {
             byte[] buffer = null;
-            var pathFile = Directory.GetCurrentDirectory() + rawUrl;
-
-            if (Directory.Exists(pathFile))
+            var filePath = _serverSetting.Path + "\\google\\index.html" + rawUrl;
+            Console.WriteLine(filePath);
+            if (Directory.Exists(filePath))
             {
-                pathFile = pathFile + "/index.html";
-                if(File.Exists(pathFile))
+                Console.WriteLine('5');
+                if (File.Exists(filePath))
                 {
-                    buffer = File.ReadAllBytes(pathFile);
+                    Console.WriteLine('6');
+                    buffer = File.ReadAllBytes(filePath);
                 }
-            }
-            else if (File.Exists(pathFile))
-            {
-                buffer = File.ReadAllBytes(pathFile);
-            }
 
-            return buffer;
+            }
+            else if (File.Exists(filePath))
+                Console.WriteLine("7");
+                buffer = File.ReadAllBytes(filePath);
+            return buffer; 
+        }
+        public void Dispose() 
+        {
+            Stop();
         }
     }
 }
